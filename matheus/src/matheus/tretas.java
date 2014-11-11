ff31353a195d4141eb94404d4fcc25b5970966d8
mattneves <matheus_neves@outlook.com>
2014-11-11 16:33:11 -0200

    Merge branch 'master' of https://github.com/mattneves/teste.git
    Conflicts:
    	matheus/src/matheus/tretas.java

---
+++ matheus/src/matheus/tretas.java
@@ -0,0 +1,14 @@
+package matheus;
+
+public class tretas {
+	
+	public String nome;
+	public String endereco;
+	public int numero;
+	public String cidade;
+	public String CEP;
+	public String pais;
+	public String nomefantasia;
+	public String hehehe;
+
+}
